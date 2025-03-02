efba9134ab51e2f31abb1f32266285745f1cc477
joaquin_n6 <joaquinne87@gmail.com>
2025-03-01 22:23:41 -0600

    get habit in calendar

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { Col, Row, Card, Calendar as CalendarAntd } from "antd";
 import markStore from "@/stores/mark.store";
+import habitStore from "@/stores/habit.store";
 
 const Calendar = () => {
   const { create, loading, createMark } = markStore();
+  const { entity, getHabit } = habitStore();
+  const [habit, setHabit] = useState(null);
   const { id } = useParams();
   const selectDay = (value) => {
     if (!id) return;
@@ -20,14 +23,27 @@ const Calendar = () => {
     }
   };
 
-  /* useEffect(() => {
-    if (create)
-    getMarks()
-  }, [create]); */
+  /*  useEffect(() => {
+    if (create) getMarks();
+  }, [create]);
 
-  /* useEffect(() => {
-    getMarks()
+  useEffect(() => {
+    getMarks();
   }, []); */
+
+  useEffect(() => {
+    if (id) {
+      getHabit(id);
+    } else {
+      setHabit(null);
+    }
+  }, [id, getHabit]);
+
+  useEffect(() => {
+    if (entity) {
+      setHabit(entity);
+    }
+  }, [entity]);
 
   return (
     <Row justify="center">
@@ -38,7 +54,10 @@ const Calendar = () => {
         lg={{ flex: "60%" }}
         xl={{ flex: "60%" }}
       >
-        <Card variant="borderless">
+        <Card
+          variant="borderless"
+          title={habit ? `Calendario de ${habit.name}` : `Calendario`}
+        >
           <CalendarAntd fullscreen onSelect={selectDay} />
         </Card>
       </Col>
