d28ceef36376bc251d23dd89b0defa8785eb1c50
joaquin_n6 <joaquinne87@gmail.com>
2025-03-08 15:34:56 -0600

    link to linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,29 @@
 import { Button } from "antd";
-import { GithubOutlined } from "@icons";
+import { GithubOutlined, LinkedinOutlined } from "@icons";
 
 const Footer = () => {
-  const onClick = () => {
-    window.open("https://github.com/joaquinn6", "_blank");
+  const onClick = (url) => {
+    window.open(url, "_blank");
   };
   return (
     <div style={{ textAlign: "center" }}>
-      Qwerty Ni code
       <Button
-        onClick={onClick}
+        onClick={() => onClick("https://github.com/joaquinn6")}
         color="primary"
         variant="link"
         shape="round"
         icon={<GithubOutlined />}
       >
         Created by joaquin_n6
+      </Button>
+      <Button
+        onClick={() => onClick("https://www.linkedin.com/in/joaquin-n6/")}
+        color="primary"
+        variant="link"
+        shape="round"
+        icon={<LinkedinOutlined />}
+      >
+        Contact me
       </Button>
     </div>
   );
