5e42ff0e2b494688b10b47c74d8e1645ef98e947
joaquin_n6 <joaquinne87@gmail.com>
2025-03-16 16:39:54 -0600

    tour cutre

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Col, Row, Card, Calendar as CalendarAntd } from "antd";
+import { Col, Row, Card, Calendar as CalendarAntd, Tour } from "antd";
 import markStore from "@/stores/mark.store";
 import habitStore from "@/stores/habit.store";
 import HabitCell from "./components/HabitCell";
@@ -11,8 +11,9 @@ import ModalMarkDetail from "./components/ModalMarkDetail";
 import utc from "dayjs/plugin/utc";
 
 //!FUTURE: creación/login con cuenta de google/facebook
+//!FUTURE: birthday event: notificación a entrar en el home-page
 //TODO: tour al ser la primera vez (supuestamente hay hooks para saber si es la primera vez)
-//TODO: birthday event: notificación a entrar en el home-page
+
 dayjs.extend(utc);
 const Calendar = () => {
   const { getMarksByHabit, getMarksByUser, list, create, update, deleted } =
@@ -30,13 +31,31 @@ const Calendar = () => {
     mark: {},
     habit: {},
   });
+  const [open, setOpen] = useState(false);
+  const ref1 = useRef(null);
   const getMarks = () => {
     if (id) getMarksByHabit(id, query);
     else getMarksByUser(query);
   };
 
+  const steps = [
+    {
+      title: "Crea una marca",
+      description:
+        "Al hacer click/tab en el día, se creara una marca nueva o se sumara una si ya existe.",
+      target: () => ref1.current,
+    },
+    {
+      title: "Editar o eliminar",
+      description:
+        "Si estas en tu celular, al mantener apretado puedes editar las marcas del día y agregar una descripción, si estas desde un ordenador, lo haces con click derecho",
+      target: () => ref1.current,
+    },
+  ];
+
   useEffect(() => {
     getMarks();
+    setTimeout(() => setOpen(true), 2000);
   }, []);
 
   useEffect(() => {
@@ -116,35 +135,39 @@ const Calendar = () => {
   };
 
   return (
-    <Row justify="center">
-      <Col
-        xs={{ flex: "100%" }}
-        sm={{ flex: "80%" }}
-        md={{ flex: "60%" }}
-        lg={{ flex: "60%" }}
-        xl={{ flex: "60%" }}
-      >
-        <Card
-          variant="borderless"
-          title={entity ? `Calendario de ${entity.name}` : `Calendario`}
+    <>
+      <Row justify="center">
+        <Col
+          xs={{ flex: "100%" }}
+          sm={{ flex: "80%" }}
+          md={{ flex: "60%" }}
+          lg={{ flex: "60%" }}
+          xl={{ flex: "60%" }}
         >
-          <CalendarAntd
-            fullscreen
-            cellRender={cellRender}
-            onPanelChange={onPanelChange}
+          <Card
+            variant="borderless"
+            title={entity ? `Calendario de ${entity.name}` : `Calendario`}
+          >
+            <CalendarAntd
+              fullscreen
+              cellRender={cellRender}
+              onPanelChange={onPanelChange}
+              ref={ref1}
+            />
+          </Card>
+        </Col>
+        {isModalOpen ? (
+          <ModalMarkDetail
+            habit={dataModal.habit}
+            mark={dataModal.mark}
+            date={dataModal.date}
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
           />
-        </Card>
-      </Col>
-      {isModalOpen ? (
-        <ModalMarkDetail
-          habit={dataModal.habit}
-          mark={dataModal.mark}
-          date={dataModal.date}
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-      ) : null}
-    </Row>
+        ) : null}
+      </Row>
+      <Tour open={open} onClose={() => setOpen(false)} steps={steps} />
+    </>
   );
 };
 export default Calendar;
